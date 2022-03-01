ebb16107b0de12849efc9ef4d38adf36c4b92c45
DEVPOB <86759611+DEVPOB@users.noreply.github.com>
2022-03-01 12:11:55 +0700

    commit add animation

--- Project-D-3D/Assets/Scripts/System/Phone_App.cs
+++ Project-D-3D/Assets/Scripts/System/Phone_App.cs
@@ -14,6 +14,30 @@ public class Phone_App : MonoBehaviour
         StartCoroutine(WaitForAnimation(Anim.GetCurrentAnimatorStateInfo(0).length));
         print("TEST");
     }
+    public void Recovery()
+    {
+        Anim.Play("RecoveryTransitionOpen");
+        onapp = true;
+        print("TEST");
+    }
+    public void Guide()
+    {
+        Anim.Play("GuideAppOpen");
+        onapp = true;
+        print("TEST");
+    }
+    public void Call()
+    {
+        Anim.Play("CALLOpen");
+        onapp = true;
+        print("TEST");
+    }
+    public void Stat()
+    {
+        Anim.Play("StatOpen");
+        onapp = true;
+        print("TEST");
+    }
     IEnumerator WaitForAnimation(float _delay =0)
     {
         yield return new WaitForSeconds(_delay=.3f);
